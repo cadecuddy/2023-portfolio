12b5bd057f2d9730b56401cf677f0602faea12bf
Cade Cuddy <cadecuddy@gmail.com>
2022-12-28 21:07:48 -0500

    create config for new experience tab + header

---
+++ src/config/experience.jsx
@@ -0,0 +1,40 @@
+const experience = [
+  {
+    company: 'Amazon',
+    title: 'Software Development Engineer Intern',
+    start: 'May 2022',
+    end: 'August 2022',
+    description: [
+      'Designed and implemented internal computer vision feedback system.',
+      'Integrated feedback feature using Go, React, and multiple AWS services.',
+      'Launched feature in prod; reported CV misclassification data on 10,000+ events/day api.',
+    ],
+    technologies: ['Go', 'Javascript', 'React', 'AWS'],
+  },
+  {
+    company: 'Liquid Rocketry Lab',
+    title: 'Software Engineer',
+    start: 'May 2020',
+    end: 'Present',
+    description: [
+      'Contributed to custom engine-testing language & compiler for a suborbital rocket CPU.',
+      'Implemented binary disassembler & test-file parser to validate language configurations.',
+      'Automated 20 hr/month engine test-file creation process.',
+    ],
+    technologies: ['Rust', 'Python'],
+  },
+  {
+    company: 'Osmosis AI',
+    title: 'Software Engineering Intern',
+    start: 'May 2019',
+    end: 'August 2019',
+    description: [
+      'Trained and tested garbage overflow detection ML model with an average 91% accuracy.',
+      'Saved time for future developers documenting internal CLI tools.',
+      'Debugged frontend UI and improved load speed by 25%.',
+    ],
+    technologies: ['Python', 'AWS'],
+  },
+]
+
+export default experience
---
+++ src/config/iconmap.jsx
@@ -0,0 +1,14 @@
+import { DiGo, DiJava, DiJsBadge, DiPython, DiReact, DiRust } from 'react-icons/di'
+import { SiAmazonaws } from 'react-icons/si'
+
+const iconMap = {
+  Go: <DiGo className='text-3xl' />,
+  JavaScript: <DiJsBadge className='text-3xl' />,
+  Python: <DiPython className='text-3xl' />,
+  React: <DiReact className='text-3xl' />,
+  Rust: <DiRust className='text-3xl' />,
+  AWS: <SiAmazonaws className='ml-1 text-3xl' />,
+  Java: <DiJava className='text-3xl' />,
+}
+
+export default iconMap
